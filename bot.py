83eba6cf7fb5f9001a14db9ab4856b098e990842
Adam Crawley <megastobartfan99@gmail.com>
2019-01-13 23:05:10 +0000

    add functions

--- bot.py
+++ bot.py
@@ -12,8 +12,12 @@ prefix = '~'
 
 Client = discord.Client()
 client = commands.Bot(command_prefix=prefix)
+client.remove_command('help')
 
 OWNER_ROLE = '500647537116708885'
+ADAM_PING = '<@!394978551985602571>'
+QOTD_BOT_CHANNEL = '533373064537440267'
+QOTD_CHANNEL = '525001381875023874'#'531461072540925982'
 
 languages = {
 ':United_Kingdom:':'gb',
@@ -48,6 +52,36 @@ languages = {
 ':Ceuta_&_Melilla:':'es'
 }
 
+#---Functions---
+def get_question_string(message):
+    language, question, author = message.content.split("\x11")
+
+    translations = []
+    for lang in ['en', 'es', 'fr', 'de', 'ja', 'ko', 'cs', 'ru', 'sv']:
+        if lang == language:
+            translations.append(question)
+        else:
+            translations.append(mt.translate(question, to_language=lang, from_language=language))
+    translations.append(author)
+
+    string = '''--------------------
+:flag_gb: {}
+:flag_es: {}
+:flag_fr: {}
+:flag_de: {}
+:flag_jp: {}
+:flag_kr: {}
+:flag_cz: {}
+:flag_rs: {}
+:flag_se: {}
+
+The question of the day was submitted by - <@{}>. If any translations are wrong, feel free to tell a member of the staff team :smiley:
+--------------------'''.format(*translations)
+
+    return string
+
+    #need to add translations to the question
+
 #---APP---
 
 @client.event
@@ -73,17 +107,37 @@ async def on_reaction_add(reaction, user):
     await client.send_message(channel, '{} has added {} to the the message {}'.format(user.name, reaction.emoji, reaction.message.content))
 
 @client.command(pass_context=True)
-async def commands(ctx):
+async def help(ctx):
     author = ctx.message.author
     embed = discord.Embed(colour=0xff8000)
 
-    msg = '''```-React with a flag to get translations
-~translations = Gets the flags that you can react with
+    msg = '''```-React with a flag to get translations (if you want more flags adding, please ping Adam C)
+
+~language_codes = Gets all the codes for languages that are used for ~translate and ~qotd
+
 ~translate <target_language> [message...]
-~translate_message <target_language> <message_id>
+
+~translate <target_language> <message_id>
+
+~qotd <current_language> [question...]
+
 ~clear <number of messages to purge, 5 is the default>```'''
 
     await client.send_message(ctx.message.channel, content=msg)
+
+@client.command(pass_context=True)
+async def language_codes(ctx):
+    await client.send_message(ctx.message.channel, content='''----*Language Codes*----
+:flag_gb: = en
+:flag_es: = es
+:flag_fr: = fr
+:flag_de: = de
+:flag_jp: = ja
+:flag_kr: = ko
+:flag_cz: = cs
+:flag_rs: = ru
+:flag_se: = sv
+''')
 
 @client.command(pass_context=True)
 async def translate(ctx, target_language='', *message):
@@ -107,17 +161,30 @@ async def translate(ctx, target_language='', *message):
             await client.send_message(ctx.message.channel, content=msg)
 
 @client.command(pass_context=True)
-async def translate_message(ctx, target_language='', message_id=''):
-    if not target_language or not message_id:
-        await client.send_message(ctx.message.channel, content='```~translate_message <target_language> <message_id>```')
+async def qotd(ctx, language, *question):
+    if not language or not question:
+        await client.send_message(ctx.message.channel, content='```~qotd <current_language> [question...]```')
     else:
-        msg = await client.get_message(ctx.message.channel, message_id)
-        trans = mt.translate(msg.content, target_language, 'auto')
+        author = ctx.message.author
+        question = ' '.join(question)
 
-        embed = discord.Embed(colour=0xff8000)
-        embed.add_field(name='Translation: '+trans, value='Original: '+msg.content, inline=False)
+        messages = [[ctx.message.channel, 'Thanks {} :P'.format(author.mention)], [client.get_channel(QOTD_BOT_CHANNEL), "{}\x11{}\x11{}".format(language, question, author.id)]]
+        for message in messages:
+            await client.send_message(message[0], content=message[1])
 
-        await client.send_message(ctx.message.channel, embed=embed)
+@client.command(pass_context=True)
+async def next_qotd(ctx):
+    if OWNER_ROLE in [y.id for y in ctx.message.author.roles]:
+        questions = []
+        async for q in client.logs_from(client.get_channel(QOTD_BOT_CHANNEL)):
+            questions.append(q)
+        questions.reverse()
+        question = questions[0]
+
+        for command in [client.delete_message(question), client.send_message(client.get_channel(QOTD_CHANNEL), content=get_question_string(question))]:
+            await command
+    else:
+        await client.send_message(ctx.message.channel, content="Hey <@{}>, you don't have sufficient permission to do this! :smaller:".format(ctx.message.author.id))
 
 @client.command(pass_context=True)
 async def clear(ctx, amount='5'):
@@ -133,7 +200,6 @@ async def clear(ctx, amount='5'):
         else:
             await client.say('Use an integer')
     else:
-        msg = "Hey <@{}>, you aren't alpha male enough to do that :smaller:".format(author.id)
-        await client.send_message(channel, msg)
+        await client.send_message(channel, "Hey <@{}>, you don't have sufficient permission to do this! :smaller:".format(author.id))
 
 client.run(os.environ.get('TOKEN'))
